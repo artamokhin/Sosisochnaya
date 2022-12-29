0ec52fef6f9d13567c278fbfb1c942b357b1aa01
Artamokhin Aleksandr <artamokhin1@yandex.ru>
2022-12-29 18:11:53 +0100

    test frontend to deploy

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
 #Если свалится одна из команд, рухнет и весь скрипт
 set -xe
-#Перезаливаем дескриптор сервиса на ВМ для деплоя
-sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
 sudo systemctl stop sausage-store-frontend
+sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
 sudo rm -rf /var/www-data/dist/frontend||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-${VERSION}.tar.gz ${NEXUS_REPO_URL_FRONTEND}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz
 sudo cp ./sausage-store-${VERSION}.tar.gz /var/www-data/dist/sausage-store-${VERSION}.tar.gz||true #"<...>||true" говорит, если команда обвалится — продолжай#Обновляем конфиг systemd с помощью рестарта
-sudo tar xzvf sausage-store-${VERSION}.tar.gz
+sudo tar xzvf sausage-store-${VERSION}.tar.gz frontend
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
 sudo systemctl restart sausage-store-frontend 
