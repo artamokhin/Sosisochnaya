9f063c4adc9f53d72f25cc739171580dec3efdcf
Artamokhin Aleksandr <artamokhin1@yandex.ru>
2023-02-20 16:14:46 +0100

    fix deploy

--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+set +e
 docker pull gitlab.praktikum-services.ru:5050/std-012-056/sausage-store/sausage-frontend:latest
 docker stop sausage-frontend || true
 docker rm sausage-frontend || true
 set -e
-docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 docker run -d --name sausage-frontend \
     --network=sausage_network \
     --restart always \
