43de064cb082646de2aad5ff8d586839737817ed
Artamokhin Aleksandr <artamokhin1@yandex.ru>
2023-03-04 17:18:10 +0100

    fix deploy by docker compose

--- report/report_deploy.sh
+++ report/report_deploy.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-MONGO_DB="mongodb:\/\/artamokhin1:***REMOVED***@rc1a-ftsq82uol4t7mcoq.mdb.yandexcloud.net:27018\/artamokhin1?tls=true&tlsAllowInvalidCertificates=true"
-echo $MONGO_DB
 docker-compose up -d report
