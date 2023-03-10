5a833ee83e1f3046383b135dc328ae5fae1d8fda
Artamokhin Aleksandr <artamokhin1@yandex.ru>
2023-03-10 14:36:46 +0100

    Blue-green

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker-compose up -d backend
+# Узнаем цвет контейнера бэка и останавливаем другой цвет
+COLOR="none"
+docker ps | grep -q blue && COLOR=blue
+docker ps | grep -q green && COLOR=green
+"$COLOR"=="blue" && docker-compose pull && docker-compose up -d --no-deps --force-recreate backend-green
