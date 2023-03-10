59ffd20c3081eb5a604c74c7b2a58d3df901f67b
Artamokhin Aleksandr <artamokhin1@yandex.ru>
2023-03-10 14:54:14 +0100

    Blue-green

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -4,3 +4,4 @@ COLOR="none"
 docker ps | grep -q blue && COLOR=blue
 docker ps | grep -q green && COLOR=green
 "$COLOR"=="blue" && docker-compose pull && docker-compose up -d --no-deps --force-recreate backend-green
+until docker container ls --filter health=healthy | grep -q "backend-green"; do sleep 1; done
