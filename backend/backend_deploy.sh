cb8fd13d380702b47d49e6ccf6607ab70e7a895a
Artamokhin Aleksandr <artamokhin1@yandex.ru>
2023-03-11 19:06:44 +0100

    Blue-green

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -6,17 +6,17 @@ docker ps | grep -q blue && BACKEND_BLUE="true"
 docker ps | grep -q green && BACKEND_GREEN="true"
 # Первичный запуск
 if [ "$BACKEND_BLUE" == "false" ] && [ "$BACKEND_GREEN" == "false" ]; then
-  docker compose up -d --force-recreate backend-blue
+  docker-compose up -d --force-recreate backend-blue
 fi
 # Если запущен синий бэкенд, то скачиваем обновление и перезапускаем зеленый
 if [ "$BACKEND_BLUE" == "true" ]; then
-  docker compose pull backend-green
-  docker compose up -d --force-recreate backend-green
+  docker-compose pull backend-green
+  docker-compose up -d --force-recreate backend-green
   #until docker container ls --filter health=healthy | grep -q "green"; do sleep 1; done
-  docker compose stop backend-blue
+  docker-compose stop backend-blue
 elif [ "$BACKEND_GREEN" == "true" ]; then
-  docker compose pull backend-blue
-  docker compose up -d --force-recreate backend-blue
+  docker-compose pull backend-blue
+  docker-compose up -d --force-recreate backend-blue
   #until docker container ls --filter health=healthy | grep -q "blue"; do sleep 1; done
-  docker compose stop backend-green
+  docker-compose stop backend-green
 fi
